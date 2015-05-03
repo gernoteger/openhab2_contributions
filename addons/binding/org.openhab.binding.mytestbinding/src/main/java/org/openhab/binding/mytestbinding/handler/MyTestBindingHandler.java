8316cab9fd407eccc6c6fd259a3e1e6628249e00
Gernot Eger <gernot.eger@gmail.com>
2015-05-03 09:01:02 +0200

    helper test binding as template for the test archetype

---
+++ addons/binding/org.openhab.binding.mytestbinding/src/main/java/org/openhab/binding/mytestbinding/handler/MyTestBindingHandler.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2014 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.mytestbinding.handler;
+
+import static org.openhab.binding.mytestbinding.MyTestBindingBindingConstants.*;
+
+import org.eclipse.smarthome.core.thing.ChannelUID;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
+import org.eclipse.smarthome.core.types.Command;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link MyTestBindingHandler} is responsible for handling commands, which are
+ * sent to one of the channels.
+ * 
+ * @author gernot - Initial contribution
+ */
+public class MyTestBindingHandler extends BaseThingHandler {
+
+    private Logger logger = LoggerFactory.getLogger(MyTestBindingHandler.class);
+
+	public MyTestBindingHandler(Thing thing) {
+		super(thing);
+	}
+
+	@Override
+	public void handleCommand(ChannelUID channelUID, Command command) {
+        if(channelUID.getId().equals(CHANNEL_1)) {
+            // TODO: handle command
+        }
+	}
+}
---
+++ addons/binding/testSkeleton.bat
@@ -0,0 +1,4 @@
+rmdir /S /Q  org.openhab.binding.mytestbinding
+create_openhab_binding_skeleton MyTestBinding mytestbinding
+cd org.openhab.binding.mytestbinding
+start cmd /K
